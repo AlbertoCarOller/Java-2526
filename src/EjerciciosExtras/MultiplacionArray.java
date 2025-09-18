ab44083231127eb9462ed94f1c95173a52777712
Alberto Carmona <carmonaolleralberto@gmail.com>
2025-09-18 13:47:09 +0200

    Ejercicio de mltiplicar adyacentes

---
+++ src/EjerciciosExtras/MultiplacionArray.java
@@ -0,0 +1,21 @@
+package EjerciciosExtras;
+
+public class MultiplacionArray {
+    public static void main(String[] args) {
+        System.out.println("Mayor multiplicación: " + multiplicarAdyscente(new int[] {1,7,3,4,5,13,7,3,9,11}));
+    }
+
+    public static int multiplicarAdyscente(int[] array) {
+        int max = 0;
+        for (int i = 0; i < array.length; i++) {
+            int mult = 0;
+            if ((i + 1) < array.length - 1) {
+                mult = array[i] * (array[i + 1]);
+                if (mult > max) {
+                    max = mult;
+                }
+            }
+        }
+        return max;
+    }
+}
