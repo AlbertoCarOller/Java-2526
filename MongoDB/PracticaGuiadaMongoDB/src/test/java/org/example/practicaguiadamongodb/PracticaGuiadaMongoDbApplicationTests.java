2a11dc8867447a917071d06521d189a3e722c6ed
Alberto Carmona Oller <carmonaolleralberto@gmail.com>
2026-02-03 14:40:26 +0100

    Demos de MongoBD antes del proyecto

---
+++ MongoDB/PracticaGuiadaMongoDB/src/test/java/org/example/practicaguiadamongodb/PracticaGuiadaMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.practicaguiadamongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaGuiadaMongoDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
